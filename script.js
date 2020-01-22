91576471034a993b73deaca4df60da84770f8cee
Ryota Kayanoma <z.ryota.kayanoma@gree.net>
2020-01-22 14:48:35 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+function client(){
+  // var connection=new WebSocket('wss://6191df17.ngrok.io');
+  var connection=new WebSocket('wss://kayanoma.github.io/websocket_server/');
+  document.getElementById('state').innerText=connection.readyState;
+  connection.send('Hello');
+  connection.onmessage=function(e){
+    document.getElementById('get').innerText=e.data;
+  };
+  connection.close();
+};
+
+
+
+// function get_permission(){
+//   if (typeof DeviceMotionEvent.requestPermission === 'function') {
+//     // iOS 13+
+//     DeviceMotionEvent.requestPermission()
+//     .then(response => {
+//       if (response == 'granted') {
+//         window.addEventListener('devicemotion', (e) => {
+//           // do something with e
+//           document.getElementById('value_x').innerText=e.acceleration.x;
+//           document.getElementById('value_y').innerText=e.acceleration.y;
+//           document.getElementById('value_z').innerText=e.acceleration.z;
+//           // e.acceleration
+//         })
+//       }
+//     })
+//     .catch(console.error)
+//   } else {
+//     // non iOS 13+
+//     document.getElementById('value_x').innerText='error not ios 13+';
+//     document.getElementById('value_y').innerText='';
+//     document.getElementById('value_z').innerText='';
+//     // document.getElementById('value_y').innerText='error not ios 13+';
+//     // document.getElementById('value_z').innerText='error not ios 13+';
+//   }
+// };
+
+
+
